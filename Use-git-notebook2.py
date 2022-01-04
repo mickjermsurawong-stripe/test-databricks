2804f6c3a9ad0c532047d671e50bf9ad0cc5a8e5
mickjermsurawong-stripe <mickjermsurawong-stripe>
2022-01-04 10:33:42 +0000

    load from auto-ml
    auto-ml is python codegen into databricks file format

--- Use-git-notebook2.py
+++ Use-git-notebook2.py
@@ -4,13 +4,54 @@
 
 # COMMAND ----------
 
-spark.conf.get("spark.app.id")
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+
+db = load_diabetes()
+X = db.data
+y = db.target
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
 
 # COMMAND ----------
 
-print("helloworld")
-print(spark)
-spark.range(1000).repartition(10).collect()
+X
+
+# COMMAND ----------
+
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+# In this run, neither the experiment_id nor the experiment_name parameter is provided. MLflow automatically creates a notebook experiment and logs runs to it.
+# Access these runs using the Experiment sidebar. Click Experiment at the upper right of this screen. 
+mlflow.set_experiment("/Users/mickjermsurawong@stripe.com/NYC-taxi-experiment")
+with mlflow.start_run():
+  n_estimators = 10
+  max_depth = 2
+  max_features = 1
+  # Create and train model
+  rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
+  rf.fit(X_train, y_train)
+  # Make predictions
+  predictions = rf.predict(X_test)
+  
+  # Log parameters
+  mlflow.log_param("num_trees", n_estimators)
+  mlflow.log_param("maxdepth", max_depth)
+  mlflow.log_param("max_feat", max_features)
+  
+  mlflow.log_param("arbitrary_param", "hello")
+  
+  # Log model
+  mlflow.sklearn.log_model(rf, "random-forest-model")
+  
+  # Create metrics
+  mse = mean_squared_error(y_test, predictions)
+    
+  # Log metrics
+  mlflow.log_metric("mse", mse)
 
 # COMMAND ----------
 

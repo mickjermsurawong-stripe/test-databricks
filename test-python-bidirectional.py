d44cc745cf09e7723d51e9666dbab9d0e13c534c
Mick Jermsurawong <mickjermsurawong@stripe.com>
2022-01-10 14:36:16 +0700

    modify python

--- test-python-bidirectional.py
+++ test-python-bidirectional.py
@@ -1,10 +1,12 @@
 # Databricks notebook source
 print("hello world from python")
+print("This line is added from IDE as real code")
 
 # COMMAND ----------
 
 # MAGIC %scala
 # MAGIC print("hello from Scala cell")
+print("This is unexpected created within Scala magic cell")
 
 # COMMAND ----------
 

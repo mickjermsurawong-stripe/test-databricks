e78200d97685333c573c731d50a251cfa036e3f1
mickjermsurawong-stripe <mickjermsurawong-stripe>
2022-01-10 07:08:11 +0000

    Export all scala

---
+++ Test-session.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %scala
+# MAGIC val a = 10
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC print(a)
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC print(a)
+
+# COMMAND ----------
+
+
